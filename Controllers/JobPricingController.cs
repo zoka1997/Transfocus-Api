319ec03197af17ceaa5c7b89283b6bff8c917413
Milos Rasovic <milos.rasovic@portfoliobi.com>
2023-03-16 14:38:28 +0100

    Aded a JobPricing helper api for job pricing modal

---
+++ Controllers/JobPricingController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TransfocusBack.DBData;
+using TransfocusBack.Models.DTO;
+using TransfocusAPI.HelperClasses;
+
+namespace TransfocusAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobPricingController : ControllerBase
+    {
+        private readonly transfocusqa20229261212Context _context;
+
+        public JobPricingController(transfocusqa20229261212Context context)
+        {
+            this._context = context;
+        }
+
+        [HttpPost]
+        public JsonResponse GetJobPricing(Guid ID)
+        {
+
+            try
+            {
+                var data = _context.JobPricings.Where(x => x.JobId == ID)
+                    .OrderBy(x => x.OrderNumber)
+                    .Select(x => new JobPricingList
+                    {
+                        PricingID = x.Id,
+                        PricingItem = x.PricingItem != null ? x.PricingItem.Description : null,
+                        Description = x.PricingItem.Description + " (" + x.PricingItem.Code + ")",
+                        Amount = x.Amount,
+                        ApplyFuelLevy = x.ApplyFuelLevy,
+                        ChargeUnit = x.PricingItem.ChargeUnitId,
+                        Quantity = x.Quantity,
+                        UnitRate = x.UnitRate
+                    });
+
+                return new JsonResponse
+                {
+                    data = data,
+                    success = true,
+                    errorMessage = null,
+                    totalCount = data.Count()
+                };
+            }
+            catch (Exception ex)
+            {
+                return new JsonResponse
+                {
+                    data = new { },
+                    errorMessage = ex.Message,
+                    success = false,
+                    totalCount = 0
+                };
+            }
+
+        }
+
+
+    }
+}
---
+++ HelperClasses/JobPricingList.cs
@@ -0,0 +1,17 @@
+﻿namespace TransfocusAPI.HelperClasses
+{
+    public class JobPricingList
+    {
+        public Guid ID { get; set; }
+        public Guid PricingID { get; set; }
+        public string PricingItem { get; set; }
+        public string Description { get; set; }
+        public decimal Quantity { get; set; }
+        public string ChargeUnit { get; set; }
+        public decimal UnitRate { get; set; }
+        public bool ApplyFuelLevy { get; set; }
+        public decimal Amount { get; set; }
+        public string PricingCodeXero { get; set; }
+        public string XeroAccountCode { get; set; }
+    }
+}
